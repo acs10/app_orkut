fab4c7a335c98ce2fbf4aec232002f80f0ab58a9
jefferson-norberto <jefferson.norbertto@gmail.com>
2022-06-05 16:29:26 -0300

    App Bar and Bottom Bar Do it

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:app_orkut/app/modules/home/teste_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'home_store.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -16,28 +18,96 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: PopupMenuButton(
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.account_circle_rounded,
+                          color: Colors.purple,
+                        ),
+                        Text("Perfil"),
+                      ],
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.settings,
+                          color: Colors.purple,
+                        ),
+                        Text("Configurações"),
+                      ],
+                    ),
+                  ),
+                ];
+              },
+              icon: const Icon(
+                Icons.supervised_user_circle,
+                size: 40,
+              ),
+            ),
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[
+                Color.fromARGB(255, 255, 64, 191),
+                Colors.purple,
+              ],
+            ),
+          ),
+        ),
+        leading: const Icon(
+          Icons.circle_outlined,
+          color: Colors.purple,
+          size: 40,
+        ),
+        title: const Text(
+          'Orkut',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      bottomNavigationBar: ConvexAppBar.badge(
+        const {2: '99+'},
+        items: const [
+          TabItem(title: 'Add', icon: Icons.add),
+          TabItem(title: 'Comunidade', icon: Icons.web),
+          TabItem(title: 'Recados', icon: Icons.chat),
+          TabItem(title: 'Amigos', icon: Icons.people),
+        ],
+        badgeColor: Colors.pinkAccent,
+        backgroundColor: Colors.purple,
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.purple,
+            Color.fromARGB(255, 255, 64, 191),
+          ],
+        ),
       ),
       body: ScopedBuilder<HomeStore, Exception, int>(
         store: store,
         onState: (_, counter) {
           return Padding(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             child: Text('$counter'),
           );
         },
-        onError: (context, error) => Center(
-          child: Text(
-            'Too many clicks',
-            style: TextStyle(color: Colors.red),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          store.increment();
-        },
-        child: Icon(Icons.add),
+        onError: (context, error) => const Center(),
       ),
     );
   }
